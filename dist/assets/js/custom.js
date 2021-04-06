ac147b5eae7d6d07e575f79f4f9754f0ab7b335f
Mahedi Amin <mahedi_cse@hotmail.com>
2021-04-06 21:54:23 +0600

    version-1

--- dist/assets/js/custom.js
+++ dist/assets/js/custom.js
@@ -63,8 +63,51 @@
       slidesToShow: 1,
       autoplay: false,
       fade: true,
-      dots: true,
+      dots: false,
+      arrows: false,
       infinite: true
     }).slickAnimation();
+  }
+
+  if ($(".t-active").length > 0) {
+    $('.t-active').slick({
+      slidesToShow: 1,
+      autoplay: false,
+      dots: false,
+      infinite: true
+    });
+  }
+
+  if ($('.wow').length) {
+    var wow = new WOW({
+      boxClass: 'wow',
+      // animated element css class (default is wow)
+      animateClass: 'animated',
+      // animation css class (default is animated)
+      offset: 0,
+      // distance to the element when triggering the animation (default is 0)
+      mobile: false,
+      // trigger animations on mobile devices (default is true)
+      live: true // act on asynchronously loaded content (default is true)
+
+    });
+    wow.init();
+  }
+
+  $(".single-rt-banner").mousemove(function (e) {
+    parallaxIt2(e, ".img-1", 40);
+    parallaxIt2(e, ".img-2", -40);
+    parallaxIt2(e, ".img-3", 20);
+    parallaxIt2(e, ".img-4", -20);
+  });
+
+  function parallaxIt2(e, target, movement) {
+    var $this = $(".single-rt-banner");
+    var relX = e.pageX - $this.offset().left;
+    var relY = e.pageY - $this.offset().top;
+    TweenMax.to(target, 1, {
+      x: (relX - $this.width() / 2) / $this.width() * movement,
+      y: (relY - $this.height() / 2) / $this.height() * movement
+    });
   }
 }(jQuery);
